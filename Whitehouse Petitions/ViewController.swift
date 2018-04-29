d95259440c2593ed821c3238cd0ea4340ca11946
Forrest Parker <forrestwparker@gmail.com>
2018-04-29 09:41:41 -0700

    Completed Section 2: Creating the basic UI: UITabBarController

--- Whitehouse Petitions/ViewController.swift
+++ Whitehouse Petitions/ViewController.swift
@@ -2,14 +2,24 @@
 //  ViewController.swift
 //  Whitehouse Petitions
 //
-//  Created by Forrest Parker on 4/28/18.
-//  Copyright © 2018 Forrest Parker. All rights reserved.
-//
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
+    var petitions = [String]()
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return petitions.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = "Title goes here"
+        cell.detailTextLabel?.text = "Subtitle goes here"
+        return cell
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
