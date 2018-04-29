3ce3b2e19d0b69d7d2200c3291648405440660b0
Forrest Parker <forrestwparker@gmail.com>
2018-04-29 10:28:53 -0700

    Completed Section 4: Rendering a petition: loadHTMLString

--- Whitehouse Petitions/ViewController.swift
+++ Whitehouse Petitions/ViewController.swift
@@ -49,6 +49,12 @@ class ViewController: UITableViewController {
         tableView.reloadData()
     }
 
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let vc = DetailViewController()
+        vc.detailItem = petitions[indexPath.row]
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
